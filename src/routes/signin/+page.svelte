380bf79a8ac74f1d81151d11424f8894c9117e6c
Raghvind Yadav <vaday.raghvind@gmail.com>
2025-02-25 16:20:21 +0530

    comment fix

--- src/routes/signin/+page.svelte
+++ src/routes/signin/+page.svelte
@@ -1,6 +1,7 @@
 <script lang="ts">
 	import { Button } from '$lib/components/ui/button';
 	import { Input } from '$lib/components/ui/input';
+	import { PasswordInput } from '$lib/components/ui/password-input';
 	import {
 		Card,
 		CardContent,
@@ -18,7 +19,6 @@
 	let password = '';
 	let loading = false;
 	let error = '';
-	let showPassword = false;
 	let loginMethod = 'password'; // default to password login
 	let verificationCode = '';
 	let showOTPInput = false;
@@ -169,56 +169,8 @@
 				{/if}
 
 				{#if loginMethod === 'password' && !showOTPInput}
-					<div class="w-ful relative flex space-x-2">
-						<Input
-							type={showPassword ? 'text' : 'password'}
-							placeholder="Password"
-							bind:value={password}
-							required
-						/>
-						<button
-							type="button"
-							class="absolute right-3 top-1/2 -translate-y-1/2"
-							on:click={() => (showPassword = !showPassword)}
-						>
-							{#if showPassword}
-								<svg
-									xmlns="http://www.w3.org/2000/svg"
-									width="20"
-									height="20"
-									viewBox="0 0 24 24"
-									fill="none"
-									stroke="currentColor"
-									stroke-width="2"
-									stroke-linecap="round"
-									stroke-linejoin="round"
-									class="text-muted-foreground"
-									><path d="M2 12s3-7 10-7 10 7 10 7-3 7-10 7-10-7-10-7Z" /><circle
-										cx="12"
-										cy="12"
-										r="3"
-									/></svg
-								>
-							{:else}
-								<svg
-									xmlns="http://www.w3.org/2000/svg"
-									width="20"
-									height="20"
-									viewBox="0 0 24 24"
-									fill="none"
-									stroke="currentColor"
-									stroke-width="2"
-									stroke-linecap="round"
-									stroke-linejoin="round"
-									class="text-muted-foreground"
-									><path d="M9.88 9.88a3 3 0 1 0 4.24 4.24" /><path
-										d="M10.73 5.08A10.43 10.43 0 0 1 12 5c7 0 10 7 10 7a13.16 13.16 0 0 1-1.67 2.68"
-									/><path
-										d="M6.61 6.61A13.526 13.526 0 0 0 2 12s3 7 10 7a9.74 9.74 0 0 0 5.39-1.61"
-									/><line x1="2" x2="22" y1="2" y2="22" /></svg
-								>
-							{/if}
-						</button>
+					<div class="space-y-2">
+						<PasswordInput bind:value={password} placeholder="Password" required />
 					</div>
 					<div class="flex items-center justify-end">
 						<Button variant="link" class="px-0 font-normal" href="/reset-password">
