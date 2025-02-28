5a5939b1118e7e9287c12343948018e7b7526a63
Jahanvi <jahanvi@Jahanvis-MacBook-Air.local>
2025-02-28 17:52:51 +0530

    changes in workflow steps

--- vite.config.ts
+++ vite.config.ts
@@ -8,6 +8,6 @@ export default defineConfig({
 		globals: true,
 		environment: 'jsdom',
 		include: ['tests/unit/*.{test,spec}.{js,ts,svelte}'],
-		setupFiles: 'tests/setup.ts',
+		setupFiles: 'tests/setup.ts'
 	}
 });
