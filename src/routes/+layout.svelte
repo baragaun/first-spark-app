44f7803ae62eb0630533bc7542a214b0b1d70d0e
rupalirajput2597 <46842523+rupalirajput2597@users.noreply.github.com>
2025-03-05 23:47:35 +0530

    feature/footer-section: implemented privacy-policy, faq, and ToS (#25)
    * added footer-section
    * implemented privacy-policy, ToS, and FAQ with dummy data
    * added dev-env.txt file
    * Updated README

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -3,17 +3,21 @@
   import * as Sidebar from '$lib/components/ui/sidebar/index.js';
   import AppSidebar from '$lib/components/app-sidebar.svelte';
   import NavBar from '@/components/nav-bar.svelte';
+  import Footer from '$lib/components/footer.svelte';
 
   let { children } = $props();
   let open = $state(true);
 </script>
 
-<div class="min-h-screen bg-background font-sans antialiased">
+<div class="flex min-h-screen flex-col bg-background font-sans antialiased">
   <Sidebar.Provider bind:open>
     <AppSidebar />
-    <main class="h-full flex-1">
+    <div class="flex flex-1 flex-col">
       <NavBar />
-      {@render children?.()}
-    </main>
+      <main class="flex flex-1 flex-col">
+        {@render children?.()}
+      </main>
+      <Footer />
+    </div>
   </Sidebar.Provider>
 </div>
