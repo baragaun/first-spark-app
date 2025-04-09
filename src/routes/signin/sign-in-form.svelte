ed9701b994b6576e70fe44111fd33010f0f5662f
Raghvind Yadav <vaday.raghvind@gmail.com>
2025-04-09 11:37:59 +0530

    sign-in-redirect

--- src/routes/signin/sign-in-form.svelte
+++ src/routes/signin/sign-in-form.svelte
@@ -358,6 +358,10 @@
           : `Enter your password to sign in as ${identifier}`;
     }
   };
+
+  const handleRedirectToSignup = () => {
+    goto('/signup');
+  };
 </script>
 
 <form method="POST" id="sign-in-form" use:enhance>
