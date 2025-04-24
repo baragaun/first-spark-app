6737d55f2db62398bab166237d8279e1db5ab48f
Rupali Rajput <rupali@Mac-510.lan>
2025-04-24 22:46:13 +0530

    handled success state on update-username and update-password

--- src/lib/components/forms/form-button.svelte
+++ src/lib/components/forms/form-button.svelte
@@ -1,25 +1,29 @@
 <script lang="ts">
   import * as Form from '$lib/components/ui/form/index.js';
-  import { LoaderCircle } from 'lucide-svelte';
+  import { Check, LoaderCircle } from 'lucide-svelte';
 
   let {
     disabled = false,
     loading = false,
+    success = false,
     buttonText = 'Submit',
     loadingText = 'Processing...',
     variant = 'default',
     fullWidth = true,
     type = 'submit',
     onClick = undefined,
+    extraClass = '',
   } = $props<{
     disabled?: boolean;
     loading?: boolean;
+    success?: boolean;
     buttonText?: string;
     loadingText?: string;
     variant?: string;
     fullWidth?: boolean;
     type?: string;
     onClick?: (() => void) | undefined;
+    extraClass?: string;
   }>();
 </script>
 
@@ -27,7 +31,7 @@
   {variant}
   {disabled}
   {type}
-  class={fullWidth ? 'w-full' : ''}
+  class={`${fullWidth ? 'w-full' : ''} ${extraClass}`}
   onclick={(e) => {
     if (onClick) {
       e.preventDefault();
@@ -38,6 +42,9 @@
   {#if loading}
     <LoaderCircle class="mr-2 animate-spin" />
     {loadingText}
+  {:else if success}
+    <Check class="mr-2 h-4 w-4" />
+    {buttonText}
   {:else}
     {buttonText}
   {/if}
