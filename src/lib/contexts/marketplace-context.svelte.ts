3f697e2a8364153aa9567cc3b81ee0773de9ba4d
Jahanvi <jahanvi@Jahanvis-MacBook-Air.local>
2025-06-04 22:11:13 +0530

    imagepath added, dropdown added, fetchcategories query added

--- src/lib/contexts/marketplace-context.svelte.ts
+++ src/lib/contexts/marketplace-context.svelte.ts
@@ -4,14 +4,51 @@ import { AppUiMessage } from '@/types/enums';
 import {
   CachePolicy,
   Vendor,
+  GiftCardProduct,
+  ProductCategory,
   type QueryOptions,
 } from '@baragaun/bg-node-client';
 import { writable } from 'svelte/store';
 let isLoading = $state(false);
-const vendorsList = writable<Vendor[]>([]);
 
 export class MarketplaceContext {
   private client = client;
+
+  async findGiftCardProducts(): Promise<GiftCardProduct[] | string | undefined> {
+    if (!this.client.isInitialized) {
+      console.error('ConversationContext.findGiftCardProducts: not initialized.');
+      return translate(AppUiMessage.systemError);
+    }
+    try {
+      isLoading = true;
+      const input = {
+        filter: {},
+        match: {},
+        options: { cachePolicy: CachePolicy.network },
+        queryOptions: {},
+      };
+      const response = await this.client.operations.giftCardProduct.findGiftCardProducts(
+        null,
+        null,
+        null,
+        input.queryOptions,
+        input.options,
+      );
+      if (!response || response.error || !response.objects) {
+        console.error('findGiftCardProducts: received error.', { response });
+        return response.error || translate(AppUiMessage.systemError);
+      }
+      return response.objects;
+    } catch (error) {
+      console.error('findGiftCardProducts: error', {
+        error: (error as Error).message,
+        stack: (error as Error).stack,
+      });
+      return translate(AppUiMessage.systemError);
+    } finally {
+      isLoading = false;
+    }
+  }
 
   async findVendors(): Promise<Vendor[] | string | undefined> {
     if (!this.client.isInitialized) {
@@ -37,12 +74,45 @@ export class MarketplaceContext {
         console.error('findVendors: received error.', { response });
         return response.error || translate(AppUiMessage.systemError);
       }
-
-      vendorsList.set(response.objects);
-
       return response.objects;
     } catch (error) {
-      console.error('FindMyChannels: error', {
+      console.error('findVendors: error', {
+        error: (error as Error).message,
+        stack: (error as Error).stack,
+      });
+      return translate(AppUiMessage.systemError);
+    } finally {
+      isLoading = false;
+    }
+  }
+
+  async findProductCategories(): Promise<ProductCategory[] | string | undefined> {
+    if (!this.client.isInitialized) {
+      console.error('ConversationContext.findProductCategories: not initialized.');
+      return translate(AppUiMessage.systemError);
+    }
+    try {
+      isLoading = true;
+      const input = {
+        filter: {},
+        match: {},
+        options: { cachePolicy: CachePolicy.network },
+        queryOptions: {},
+      };
+      const response = await this.client.operations.productCategory.findProductCategories(
+        null,
+        null,
+        null,
+        input.queryOptions,
+        input.options,
+      );
+      if (!response || response.error || !response.objects) {
+        console.error('findProductCategories: received error.', { response });
+        return response.error || translate(AppUiMessage.systemError);
+      }
+      return response.objects;
+    } catch (error) {
+      console.error('findProductCategories: error', {
         error: (error as Error).message,
         stack: (error as Error).stack,
       });
