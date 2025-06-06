4987289f7b80775741bd92f93d68c78cffe95d2d
Jahanvi <jahanvi@Jahanvis-MacBook-Air.local>
2025-06-06 14:24:16 +0530

    new theme added

--- src/lib/components/layout/nav-bar/light-switch.svelte
+++ src/lib/components/layout/nav-bar/light-switch.svelte
@@ -3,7 +3,7 @@
   import * as DropdownMenu from '$lib/components/ui/dropdown-menu';
   import * as Tooltip from '@/components/ui/tooltip';
   import { m } from '$lib/paraglide/messages.js';
-  import { Check, Moon, Sun } from 'lucide-svelte';
+  import { Check, Moon, Sun, Palette } from 'lucide-svelte';
   import { resetMode, setMode, userPrefersMode } from 'mode-watcher';
   const { class: className = '', iconButton = true } = $props();
 
@@ -11,6 +11,7 @@
     { value: 'light', label: m['light_switch.light'](), action: () => setMode('light') },
     { value: 'dark', label: m['light_switch.dark'](), action: () => setMode('dark') },
     { value: 'system', label: m['light_switch.system'](), action: () => resetMode() },
+    { value: 'kcu', label: m['light_switch.kcu'](), action: () => setMode('kcu') },
   ];
 </script>
 
@@ -26,10 +27,13 @@
             aria-label={m['light_switch.tooltip']()}
           >
             <Sun
-              class="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0"
+              class="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0 kcu:-rotate-90 kcu:scale-0"
             />
             <Moon
-              class="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100"
+              class="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100 kcu:rotate-90 kcu:scale-0"
+            />
+            <Palette
+              class="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-90 dark:scale-0 kcu:rotate-0 kcu:scale-100"
             />
             {#if !iconButton}
               <span class="ml-2">{m['light_switch.tooltip']()}</span>
