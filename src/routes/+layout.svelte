be5a50d09ef858c2ef381fee3a397ddb71f0870e
Jahanvi <jahanvi@Jahanvis-MacBook-Air.local>
2025-06-11 13:03:17 +0530

    dynamic favicon and title changes

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -13,6 +13,8 @@
   let { children } = $props();
 </script>
 
+<MetaTags {title} {description} {canonicalUrl} />
+
 <MyUserProvider>
   <div class="flex min-h-screen flex-col bg-background font-sans antialiased">
     <ModeWatcher />
