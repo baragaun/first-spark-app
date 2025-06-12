bd8523c9750493403647d2e7c48b4ddc5e83c232
Jahanvi <jahanvi@Jahanvis-MacBook-Air.local>
2025-06-12 09:51:38 +0530

    dynamic app name added in content

---
+++ src/lib/stores/app-store.ts
@@ -0,0 +1,50 @@
+import { env } from '$env/dynamic/public';
+import { FirstSparkApp, KCUApp } from '@/types/enums';
+import { derived, writable } from 'svelte/store';
+
+// Create a derived store for the app title
+export const appTitle = derived(writable(env.PUBLIC_PROJECTNAME), ($projectName) => {
+  switch ($projectName) {
+    case 'FirstSpark':
+      return FirstSparkApp.title;
+    case 'KCU':
+      return KCUApp.title;
+    default:
+      return 'First Spark';
+  }
+});
+
+// Create a derived store for the app description
+export const appDescription = derived(writable(env.PUBLIC_PROJECTNAME), ($projectName) => {
+  switch ($projectName) {
+    case 'FirstSpark':
+      return FirstSparkApp.description;
+    case 'KCU':
+      return KCUApp.description;
+    default:
+      return '';
+  }
+});
+
+// Create a derived store for the canonical URL
+export const appCanonicalUrl = derived(writable(env.PUBLIC_PROJECTNAME), ($projectName) => {
+  switch ($projectName) {
+    case 'FirstSpark':
+      return FirstSparkApp.canonicalUrl;
+    case 'KCU':
+      return KCUApp.canonicalUrl;
+    default:
+      return '';
+  }
+});
+
+export const headerIcon = derived(writable(env.PUBLIC_PROJECTNAME), ($projectName) => {
+  switch ($projectName) {
+    case 'FirstSpark':
+      return '/fs-logo.svg';
+    case 'KCU':
+      return '/favicon-kcu.png';
+    default:
+      return '/fs-logo.svg';
+  }
+});
