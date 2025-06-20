356be541fe43a0244d5625296e5ed30dcf7e82d3
Jahanvi <jahanvi@Jahanvis-MacBook-Air.local>
2025-06-20 17:02:09 +0530

    UI changes, html support added

--- src/routes/api/generate-pdf/+server.ts
+++ src/routes/api/generate-pdf/+server.ts
@@ -1,5 +1,5 @@
-import type { RequestHandler } from '@sveltejs/kit';
 import chromium from '@sparticuz/chromium';
+import type { RequestHandler } from '@sveltejs/kit';
 import puppeteer from 'puppeteer-core';
 
 export const POST: RequestHandler = async ({ request }) => {
