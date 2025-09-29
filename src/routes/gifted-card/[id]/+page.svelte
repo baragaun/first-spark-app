fec4c83af3c3567082dddd7a15377d28be3071af
Jahanvi <jahanvi@Jahanvis-MacBook-Air.local>
2025-09-29 14:08:03 +0530

    fixed the issue of already accepted gift card

--- src/routes/gifted-card/[id]/+page.svelte
+++ src/routes/gifted-card/[id]/+page.svelte
@@ -24,6 +24,7 @@
   let product = $state<GiftCardProduct | undefined>(undefined);
   let brand = $state<Brand | undefined>(undefined);
   let walletItemTransferAcceptInfo = $state<WalletItemTransferAcceptInfo | undefined>(undefined);
+  let isGiftCardAlreadyAccepted = $state(false);
 
   const transferSlug = page.params.id;
 
@@ -67,9 +68,16 @@
     try {
       const response =
         await marketplaceContext.findWalletItemTransferAcceptInfoByTransferSlug(transferSlug);
-
-      if (typeof response === 'string') {
+      if (typeof response === 'string' || response === null) {
+        isLoading = false;
+        isGiftCardAlreadyAccepted = true;
         logger.error('Failed to load wallet item', response);
+        return;
+      }
+
+      if (response?.walletItem === null || response?.product === null) {
+        isGiftCardAlreadyAccepted = true;
+        isLoading = false;
         return;
       }
 
@@ -88,67 +96,75 @@
   });
 </script>
 
-<!-- Header Bar -->
-<div
-  class="flex items-center justify-between rounded-b-lg bg-nav px-4 py-3 text-nav-foreground shadow"
->
-  <span class="text-lg font-bold"> {m['send_gift_card.received_gift_card']()}</span>
+{#if isGiftCardAlreadyAccepted}
+  <div class="flex h-[60vh] flex-col items-center justify-center">
+    <span class="px-8 text-center text-lg font-bold text-primary">
+      {m['gifted_card.already_accepted_message']()}
+    </span>
+  </div>
+{:else}
+  <!-- Header Bar -->
+  <div
+    class="flex items-center justify-between rounded-b-lg bg-nav px-4 py-3 text-nav-foreground shadow"
+  >
+    <span class="text-lg font-bold"> {m['send_gift_card.received_gift_card']()}</span>
 
-  {#if !verified}
-    <div class="flex gap-2">
-      <Button
-        variant="outline"
-        size="sm"
-        class="rounded-full hover:bg-background hover:text-nav-foreground/70"
-        onclick={() => {
-          open = true;
-        }}>{m['gifted_card.accept']()}</Button
-      >
-      <Button
-        variant="outline"
-        size="sm"
-        class="rounded-full border-red-600 text-red-700 hover:bg-background hover:text-red-500"
-        onclick={() => {
-          declineWalletItemTransfer();
-        }}>{m['gifted_card.decline']()}</Button
-      >
+    {#if !verified}
+      <div class="flex gap-2">
+        <Button
+          variant="outline"
+          size="sm"
+          class="rounded-full hover:bg-background hover:text-nav-foreground/70"
+          onclick={() => {
+            open = true;
+          }}>{m['gifted_card.accept']()}</Button
+        >
+        <Button
+          variant="outline"
+          size="sm"
+          class="rounded-full border-red-600 text-red-700 hover:bg-background hover:text-red-500"
+          onclick={() => {
+            declineWalletItemTransfer();
+          }}>{m['gifted_card.decline']()}</Button
+        >
+      </div>
+    {/if}
+  </div>
+
+  {#if isLoading}
+    <div class="flex h-[60vh] items-center justify-center">
+      <div
+        class="h-8 w-8 animate-spin rounded-full border-4 border-primary border-t-transparent"
+      ></div>
     </div>
   {/if}
-</div>
 
-{#if isLoading}
-  <div class="flex h-[60vh] items-center justify-center">
-    <div
-      class="h-8 w-8 animate-spin rounded-full border-4 border-primary border-t-transparent"
-    ></div>
-  </div>
+  {#if product && brand}
+    <GiftCardDetails
+      walletItem={acceptedWalletItem ?? walletItemTransferAcceptInfo?.walletItem}
+      {product}
+      {brand}
+      showNavBar={false}
+      hideActions={true}
+      isVerified={verified}
+    />
+  {/if}
+
+  <Dialog bind:open>
+    <DialogContent>
+      <DialogHeader>
+        <DialogTitle>{m['gifted_card.modal_title']()}</DialogTitle>
+      </DialogHeader>
+
+      <form class="space-y-4" onsubmit={handleSubmit}>
+        <Input
+          type="password"
+          class="focus-visible:outline-none  focus-visible:ring-white"
+          placeholder={m['gifted_card.pin_placeholder']()}
+          bind:value={pin}
+        />
+        <Button type="submit" class="w-full">{m['gifted_card.submit']()}</Button>
+      </form>
+    </DialogContent>
+  </Dialog>
 {/if}
-
-{#if product && brand}
-  <GiftCardDetails
-    walletItem={acceptedWalletItem ?? walletItemTransferAcceptInfo?.walletItem}
-    {product}
-    {brand}
-    showNavBar={false}
-    hideActions={true}
-    isVerified={verified}
-  />
-{/if}
-
-<Dialog bind:open>
-  <DialogContent>
-    <DialogHeader>
-      <DialogTitle>{m['gifted_card.modal_title']()}</DialogTitle>
-    </DialogHeader>
-
-    <form class="space-y-4" onsubmit={handleSubmit}>
-      <Input
-        type="password"
-        class="focus-visible:outline-none  focus-visible:ring-white"
-        placeholder={m['gifted_card.pin_placeholder']()}
-        bind:value={pin}
-      />
-      <Button type="submit" class="w-full">{m['gifted_card.submit']()}</Button>
-    </form>
-  </DialogContent>
-</Dialog>
